3e842ec58379169b728c66b3e3a772f8907f9cd4
Yoshihiko Kanaumi <yoshihiko@gmail.com>
2016-03-07 17:42:20 +0900

    modify northstar.py

--- northstar-rest_api-test/northstar.py
+++ northstar-rest_api-test/northstar.py
@@ -24,7 +24,7 @@ def error_exit (estr, code) :
 
 def northstar_api (suffix):
 	r = requests.get (urlprefix + suffix, headers = headers, auth = authkey, verify = False)
-	if r.status_code != 200 :
+	if r.status_code != 200:
 		error_exit ("Error %d: %s" % (r.status_code, r.text), r.status_code)
 	return r.json ()
 
@@ -37,14 +37,20 @@ def northstar_api_no_data_put (suffix) :
 def northstar_api_post (suffix, dictdata) :
 	r = requests.post (urlprefix + suffix, data = json.dumps (dictdata), headers = headers, 
 			auth = authkey, verify = False)
-	if r.status_code != 200 :
+	if r.status_code != (200 and 201) :
 		error_exit ("Error %d: %s" % (r.status_code, r.text), r.status_code)
 	return r.text
 
 def northstar_api_put (suffix, dictdata) :
 	r = requests.put (urlprefix + suffix, data = json.dumps (dictdata), headers = headers,
 			 auth = authkey, verify = False)
-	if r.status_code != 200 :
+	if r.status_code != (200 and 201) :
+		error_exit ("Error %d: %s" % (r.status_code, r.text), r.status_code)
+	return r.text
+
+def northstar_api_no_data_delete (suffix) :
+	r = requests.delete (urlprefix + suffix, auth = authkey, headers = headers, verify = False)
+	if r.status_code != 204 :
 		error_exit ("Error %d: %s" % (r.status_code, r.text), r.status_code)
 	return r.text
 
@@ -57,6 +63,28 @@ def lsp_list (args) :
 	suffix = "/NorthStar/API/v1/tenant/1/topology/1/te-lsps" 
 	res = northstar_api (suffix)
 	dictdumper (res)
+
+def lsp_search (args) :
+        # lsp_search param 
+        try :
+                (lsp_search, param) = args
+        except :
+                error_exit ("invalid syntax %s" % ' '.join (args), -1)
+
+        suffix = "/NorthStar/API/v1/tenant/1/topology/1/te-lsps/search?name=%s" % param
+        res = northstar_api (suffix)
+        dictdumper (res)
+
+def del_lsp (args) :
+    # del-vbr vtnname vbrname
+	try :
+		(lsp_del, lspIndex) = args 
+	except :
+		error_exit ("invalid syntax %s" % ' '.join (args), -1)
+
+	suffix = "/NorthStar/API/v1/tenant/1/topology/1/te-lsps/%s" % lspIndex 
+	res = northstar_api_no_data_delete (suffix)
+
 
 def nodes_list (args) :
         suffix = "/NorthStar/API/v1/tenant/1/topology/1/nodes"
@@ -97,9 +125,9 @@ def create_lsp(args):
 	print res
 
 def explicit_lsp(args):
-        # explicit_lsp lsp_name From_ipadd To_ipadd Bandwidth
+        # explicit_lsp lsp_name From_ipadd To_ipadd BW Via_ipaddr 
         try :
-                (explicit_lsp, lsp_name, from_ipaddr, to_ipaddr, via_ipaddr, BW) = args
+               	(explicit_lsp, lsp_name, from_ipaddr, to_ipaddr, BW, via_ipaddr) = args
         except :
                 error_exit ("invalid syntax %s" % ' '.join (args), -1)
 
@@ -120,22 +148,23 @@ def explicit_lsp(args):
 			"holdingPriority": 7,
 			"ero" : [ {
 				"topoObjectType": "ipv4",
-				"address": via_ipaddr
-	#			"loose": "true"
+				"address": via_ipaddr,
 				}
 			]
 		}
 	}
-	print data
-
         res = northstar_api_post (suffix, data)
         print res
 
 
 def usage ():
+	print " get_topology : Get topology with BGP-LS"
+	print " nodes_list : Get nodes list"
+	print " links_list : Get links list"
 	print " lsp_list : Get LSP list"
 	print " create_lsp [LSP name] [From] [To] [Bandwidth] : create LSP without explicit route"
-	print " explicit_lsp [LSP name] [From] [To] [via] [Bandwidth] : create LSP with explicit route"
+	print " explicit_lsp [LSP name] [From] [To] [Bandwidth] [via] : create LSP with explicit route"
+	print " lsp_search [parameter] : search LSP with parameter (name, ip address and so on?)"
 
 
 def main():
@@ -153,6 +182,8 @@ def main():
 		"lsp_list" : lsp_list,
 		"create_lsp" : create_lsp,
 		"explicit_lsp": explicit_lsp,
+		"lsp_search": lsp_search,
+		"del_lsp": del_lsp,
 	}
 
 	if not commands.has_key (args[0]) :
